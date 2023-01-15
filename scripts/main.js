9d0dbd4c64e44a2190754d6029b4acffc99e5329
pepters <108479608+pepters@users.noreply.github.com>
2023-01-15 18:36:33 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+Events.on(UnitDestroyEvent, event => {
+  // display toast on top of screen when the unit was a player
+  if(event.unit.isPlayer()){
+    Vars.ui.hudfrag.showToast("Pathetic.");
+  }
+})
